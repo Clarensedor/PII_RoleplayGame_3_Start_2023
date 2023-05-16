2ae76bd03990e831f23cbde42aa596fd42c4b47a
Ramirods <ramiro.dasilva@correo.ucu.edu.uy>
2023-05-16 11:50:33 -0300

    carlos enemy added

---
+++ src/Library/Characters/Enemies/Carlos.cs
@@ -0,0 +1,16 @@
+using RoleplayGame;
+
+namespace RoleplayGame
+{
+    public class Carlos : Enemies
+    {
+        private int vp;
+
+        public Carlos(string name) : base(name)
+        {
+            this.vp = 10;
+            this.AddItem(new Staff());
+            this.AddItem(new Helmet());
+        }
+    }
+}
